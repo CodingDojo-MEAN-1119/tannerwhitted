0e4772eb0894b19186f092d9fe9d04bdc08a066c
tannerwhitted <tannerwhitted@gmail.com>
2019-11-06 19:25:32 -0800

    assignments completed

--- 04-mongodb/02-mongoose/01-quoting-dojo/server.js
+++ 04-mongodb/02-mongoose/01-quoting-dojo/server.js
@@ -1,38 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const path = require('path');
 const port = 8000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/quoting_dojo_app');
-
-const quoteSchema = new mongoose.Schema({
-  name: String,
-  quote: String
-});
-
-const Quote = mongoose.model('quotes', quoteSchema);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-app.get('/quotes', function(req, res) {
-  Quote.find({}, function(err, quotes) {
-    if (err) { console.log(err); }
-    res.render('quotes', { quotes: quotes });
-  });
-});
-
-app.post('/quotes', function(req, res) {
-  Quote.create(req.body, function(err) {
-    if (err) { console.log(err); }
-    res.redirect('/quotes');
-  });
-});
+require('./server/config/mongoose.js')
+require('./server/config/routes.js')(app)
 
 app.listen(port);
---
+++ 04-mongodb/02-mongoose/01-quoting-dojo/server/config/routes.js
@@ -0,0 +1,6 @@
+const quotesController = require('../controllers/quotes.controller.js')
+module.exports = function(app){
+    app.get('/',  quotesController.index),
+    app.post('/quotes', quotesController.create),
+    app.get('/quotes', quotesController.quotes_page)
+};
